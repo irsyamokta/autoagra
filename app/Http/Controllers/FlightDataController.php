67c4125c4fa95d7b4dc6d1d81bd64f93a07e771c
weedstream <dede.ipang426@gmail.com>
2024-10-28 22:16:40 +0700

    admin: fix database and show the data to fe

--- app/Http/Controllers/FlightDataController.php
+++ app/Http/Controllers/FlightDataController.php
@@ -14,23 +14,38 @@ class FlightDataController extends Controller
 
     public function store(Request $request)
     {
-        // Validate incoming data
         $validatedData = $request->validate([
             'flight_time' => 'required|integer|min:0',
             'pesticide_liters' => 'required|numeric|min:0',
             'number_of_flights' => 'required|integer|min:0',
         ]);
 
-        // Create a new FlightData record
         FlightData::create($validatedData);
 
-        // Redirect to index with success message
         return redirect()->route('admin.index')->with('success', 'Data successfully added!');
     }
 
     public function index()
     {
         $flightData = FlightData::all();
-        return view('admin.index', compact('flightData'));
+
+        $totals = [
+            'totalFlightTime' => $flightData->sum('flight_time'),
+            'totalPesticideLiters' => $flightData->sum('pesticide_liters'),
+            'totalNumberOfFlights' => $flightData->sum('number_of_flights'),
+        ];
+
+        return view('admin.index', compact('flightData', 'totals'));
+    }
+
+    public function showIndex()
+    {
+        $flightData = FlightData::all();
+        $totals = [
+            'totalFlightTime' => $flightData->sum('flight_time'),
+            'totalPesticideLiters' => $flightData->sum('pesticide_liters'),
+            'totalNumberOfFlights' => $flightData->sum('number_of_flights'),
+        ];
+        return view('frontend.index', compact('flightData', 'totals'));
     }
 }
