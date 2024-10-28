6bbd0ce7e0b162fd468668c56e067041edcbdf35
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2024-10-28 20:30:45 +0700

    Fix : About section

--- resources/views/frontend/section/about.blade.php
+++ resources/views/frontend/section/about.blade.php
@@ -5,6 +5,6 @@
     </div>
     <div class="w-5/12 flex justify-center lg:justify-end items-center relative">
         <img src="{{ asset('assets/img/img-about.png') }}" class="w-[350px] md:w-[600px] lg:w-[700px] xl:w-[800px] 2xl:w-[900px] max-w-none lg:absolute lg:-right-[15px] lg:-top-[300px] xl:-right-[10px] xl:-top-[350px] 2xl:-right-[5px] 2xl:-top-[400px] z-50" alt="Drone Autoagra">
-        <img src="{{ asset('assets/icon/icon-circle.png') }}" class="absolute w-72 md:w-[500px] max-w-none top-[200px] -right-[200px] md:top-[350px] md:-right-[400px] lg:top-[200px] lg:-right-[800px] xl:top-[180px] xl:-right-[800px] 2xl:top-[160px] 2xl:-right-[900px] " alt="circle">
+        <img src="{{ asset('assets/icon/icon-circle.png') }}" class="absolute w-72 md:w-[500px] max-w-none top-[250px] -right-[200px] md:top-[350px] md:-right-[400px] lg:top-[200px] lg:-right-[800px] xl:top-[180px] xl:-right-[800px] 2xl:top-[160px] 2xl:-right-[900px] " alt="circle">
     </div>
 </section>
