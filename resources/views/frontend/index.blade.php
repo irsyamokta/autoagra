57ea99f1c1f50dea7cb5619643da1ce5cb745a09
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2024-10-28 19:52:18 +0700

    Add : 3D View Section

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -3,6 +3,7 @@
     @include('frontend.section.hero')
     @include('frontend.section.about')
     @include('frontend.section.spesification')
+    @include('frontend.section.view')
     @include('frontend.section.gallery')
     @include('frontend.section.report')
     @include('frontend.components.scroll')
---
+++ resources/views/frontend/section/view.blade.php
@@ -0,0 +1,6 @@
+<section id="view" class="flex flex-col  justify-center items-center h-[100vh] bg-white overflow-hidden">
+    <div class="flex flex-col justify-center items-center gap-5">
+        <h1 class="md:mb-4 text-2xl font-semibold tracking-wide text-black md:text-5xl xl:text-6xl text-wrap">3D View 360°</h1>
+        <div class="sketchfab-embed-wrapper"> <iframe title="Autoagra" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share width="1900" height="600" src="https://sketchfab.com/models/d55b4c66ccbe4fd1b8f6f3886b7da68c/embed?autostart=1"> </iframe> </div>
+    </div>
+</section>
